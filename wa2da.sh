9d79074841c788ccd052340b46bb5f73de6a543d
AinTEAsports <benkirat.khalil@gmail.com>
2023-08-17 20:24:53 +0200

    added the actual script

---
+++ wa2da.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# You can modify this
+NATIVEFIER_APPS_DIRECTORY="${HOME}/.nativefier-apps"
+
+# ------- #
+
+APP_NAME="${1}"
+WEBSITE_URL="${2}"
+APP_DIRECTORY="${APP_NAME}-linux-x64"
+
+DESKTOP_FILENAME=$(echo "${APP_NAME}.desktop" | tr '[:upper:]' '[:lower:]')
+ABSOLUTE_FILENAME="${HOME}/.local/share/applications/${DESKTOP_FILENAME}"
+
+
+show_help() {
+	echo "\
+Use: ${0} <app_name> <website_url>
+\"Downloads\" a website into a desktop app, using \"nativefier\"\
+"
+}
+
+
+create_app() {
+	nativefier --name "${APP_NAME}" "${WEBSITE_URL}" 0>/tmp/make-webapp.log 1>/tmp/make-webapp.log 2>/tmp/make-webapp.log || exit 1
+}
+
+
+move_app_to_nativfier_app_directory() {
+	mv "${PWD}/${APP_DIRECTORY}" "${NATIVEFIER_APPS_DIRECTORY}/${APP_DIRECTORY}"
+}
+
+
+make_config() {
+	echo -n "
+[Desktop Entry]
+Type=Application
+Exec=${NATIVEFIER_APPS_DIRECTORY}/${APP_DIRECTORY}/${APP_NAME}
+# Icon=Alacritty
+Terminal=false
+
+Name=${APP_NAME}
+GenericName=${APP_NAME}
+# Comment=
+StartupWMClass=${APP_NAME}
+Actions=New;
+
+[Desktop Action New]
+Name={APP_NAME}
+Exec=${NATIVEFIER_APPS_DIRECTORY}/${APP_DIRECTORY}/${APP_NAME}
+"
+}
+
+
+make_desktop_file() {
+	make_config > "${ABSOLUTE_FILENAME}"
+}
+
+
+main() {
+	if [[ -z $(command -v nativefier) ]]; then
+		echo "'nativefier' could not be found, please install it"
+		exit 1
+	fi
+
+	if [[ "$#" -ne 2 ]]; then
+		show_help
+		exit 1
+	fi
+
+	if [[ -d "${NATIVEFIER_APPS_DIRECTORY}/${APP_DIRECTORY}" ]]; then
+		>&2 echo "[ERROR] Application '${APP_NAME}' already exists"
+		exit 1
+	fi
+
+	if [[ -f "${ABSOLUTE_FILENAME}" ]]; then
+		>&2 echo "[ERROR] '${ABSOLUTE_FILENAME}' already exists"
+		exit 1
+	fi
+
+	create_app
+	make_desktop_file
+	move_app_to_nativfier_app_directory
+
+	echo "Application '${APP_NAME}' successfully created at '${NATIVEFIER_APPS_DIRECTORY}/${APP_DIRECTORY}'"
+}
+
+
+main $@
